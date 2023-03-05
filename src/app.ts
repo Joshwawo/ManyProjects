b33793d1be0daf7d6722ad870bf38d8bf5f3e42d
Jorge Alberto  Morales Gutierrez <L17330479@hermosillo.tecnm.mx>
2023-03-05 10:35:21 -0700

    hotfix bugsillo in auth

--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,19 @@
 import "dotenv/config";
-import express from "express";
+import express , {Request, Response} from "express";
 import cors from "cors";
 import morgan from "morgan";
 import fileupload from "express-fileupload";
 import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
 import helmet from "helmet";
 import compression from "compression";
 import axios, { AxiosResponse } from "axios";
 import colors from 'colors'
-
 import { router } from "./routes";
 import { dbConnect } from "./config/mongo";
 import swagger_pathJson from "./utils/swaggerJson.json";
 import { convertMsToMIn } from "./helpers/convertMsToMins";
-import path from "node:path";
+import checkAuth from './middlewares/checkAuth'
 
-// const swaggerDocument = YAML.load('./swagger.yaml');
-// const swagger_path = path.resolve(__dirname, "./utils/swagger.yaml");
-// const swaggerDocument = YAML.load(swagger_path);
-// console.log(swagger_path);
-// console.log(swagger_pathJson);
-
-export const swaggerSpec: swaggerJsDoc.Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API DOCUMENTATION",
-      version: "1.0.0",
-      description: "API DOCUMENTATION FOR THE THIS APP",
-    },
-    servers: [
-      {
-        url: "http://localhost:3003",
-      },
-      {
-        url: "https://api-projects.up.railway.app",
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-  },
-  apis: ["./src/routes/*.ts", "./src/routes/*.js"],
-  // apiss: [`${path.join(__dirname, "./routes/*.ts")}`],
-  // apis: [`${path.join(__dirname, "./routes/*.ts", "./routes/*.js")}`],
-};
 
 const PORT = process.env.PORT || 3000;
 
@@ -93,13 +55,8 @@ app.use(
   })
 );
 app.use(router);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerJsDoc(swaggerSpec))
-);
-// app.use("/api-yaml", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api-json", swaggerUi.serve, swaggerUi.setup(swagger_pathJson));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger_pathJson));
 const call = async (): Promise<void> => {
   console.log('Entre'.white)
   try {
@@ -119,6 +76,11 @@ const call = async (): Promise<void> => {
     console.log("uno un error en la fn call".red);
   }
 };
+
+app.use("*",checkAuth,(_req:Request, res:Response)=>{
+  res.json({message: 'This route does exist'})
+})
+
 // const pathIn = path.join(__dirname, "../../towebp.jpg");
 //     console.log(pathIn)
 
