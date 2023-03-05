ad3d35e2b1504124279d56450dc18051d8ee19b1
Jorge Alberto  Morales Gutierrez <L17330479@hermosillo.tecnm.mx>
2023-03-05 10:05:14 -0700

    hotfix bugsillo in auth

--- src/utils/generateJWT.ts
+++ src/utils/generateJWT.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateJWT = (id: object) => {
   return jwt.sign({ id }, String(process.env.JWT_SECRET), {
-    expiresIn: "30d",
+    // expiresIn: "30d",
   });
 };
 
